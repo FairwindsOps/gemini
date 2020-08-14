eac0130cf519ece975a2ea8273d115b15bcbe5b4
Robert Brennan <contact@rbren.io>
2020-08-14 18:32:55 +0000

    add auth package

--- pkg/kube/client.go
+++ pkg/kube/client.go
@@ -8,6 +8,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/restmapper"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 
